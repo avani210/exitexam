f07a2691f990fb2e522cfcab0ebc3e44dd2dba4a
avani210 <aavanitm@gmail.com>
2025-07-02 21:20:00 +0530

    Add files via upload

---
+++ a1.js
@@ -0,0 +1,12 @@
+let person={
+    name:"Antonio",
+    age: 12 ,
+    place:"TCR",
+    address:{
+        street:"any" ,
+        housename:"AA"
+    }
+};
+console.log(person);
+console.log(person.name);
+console.log(person.address.housename);
